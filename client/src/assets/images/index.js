37232d9bd17698f51033699ec5c5afa80bc7c98f
mchlbataller <mchlbataller@gmail.com>
2020-06-05 19:09:32 +0800

    Added the My Apps listing

---
+++ client/src/assets/images/index.js
@@ -0,0 +1,3 @@
+export { default as portal } from "./portal.png";
+export { default as assessment } from "./assessment.png";
+export { default as projectDapo } from "./projectDapo.png";
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,3 +1,5 @@
+import { assessment, portal, projectDapo } from "../assets/images";
+
 import { ProgressBar } from "../components";
 import React from "react";
 import Typist from "react-typist";
@@ -117,6 +119,92 @@ const Landing = (props) => {
 						label={element.label}
 					/>
 				))}
+			</Card>
+
+			<Card height="1200px">
+				<CardHeader>My Apps</CardHeader>
+				<br />
+				<br />
+				{/* Web Portal App */}
+				<div>
+					<div className="grid grid-cols-2 gap-2">
+						<div className="leading-tight">
+							<p>In the works:</p>
+							<p className="text-xl font-light">Web Portal App</p>
+							<p
+								className="text-blue-600 text-xs cursor-pointer"
+								onClick={(e) =>
+									window.open(
+										"https://portal.lynx-web.systems"
+									)
+								}
+							>
+								portal.lynx-web.systems
+							</p>
+							<br />
+							<p className="text-sm font-regular">
+								A web app that displays all of your subscribed
+								apps.
+								<br />
+								<br />
+								People won’t have to remember the web apps by
+								mind and type ‘facebook.com’ or ‘youtube.com’ in
+								the browser again.
+							</p>
+						</div>
+						<div>
+							<img src={portal} alt="portal" width="100%" />
+						</div>
+					</div>
+				</div>
+
+				{/* Assessment App */}
+				<div className="leading-tight text-center mt-16">
+					<img
+						src={assessment}
+						alt="assessment"
+						className="w-3/4 mx-auto"
+					/>
+					<p className="text-xl font-light mt-3">
+						Xavier School Nuvali <br /> Assessment App
+					</p>
+					<p className="text-xs">
+						App is currently not available for viewing
+					</p>
+					<p className="text-sm mt-2 w-5/6 mx-auto font-regular">
+						An app that enables the enrollees to select School
+						Supplies, Books, and Tuition Fees then the app gives a
+						printable PDF for later assessment.
+					</p>
+				</div>
+
+				{/* Project DAPO App */}
+				<div className="leading-tight mt-16">
+					<div className="grid grid-cols-2 gap-2">
+						<div>
+							<img src={projectDapo} alt="projectDapo" />
+						</div>
+						<div>
+							<br />
+							<br />
+							<p className="text-xl font-light mt-3">
+								Project DAPO App
+							</p>
+							<p
+								className="text-blue-600 text-xs cursor-pointer"
+								onClick={(e) =>
+									window.open("https://dapo.bnshosting.net")
+								}
+							>
+								dapo.bnshosting.net
+							</p>
+							<p className="text-sm mt-2 mx-auto font-regular">
+								A web system that displays the project’s
+								homepage and tracks the collected data.
+							</p>
+						</div>
+					</div>
+				</div>
 			</Card>
 		</div>
 	);
