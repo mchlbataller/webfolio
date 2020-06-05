032105c59b42dc02b265236434c627971391e32e
mchlbataller <mchlbataller@gmail.com>
2020-06-05 20:02:05 +0800

    Added Error message for high dpis

--- client/src/assets/images/index.js
+++ client/src/assets/images/index.js
@@ -1,3 +1,4 @@
 export { default as portal } from "./portal.png";
 export { default as assessment } from "./assessment.png";
 export { default as projectDapo } from "./projectDapo.png";
+export { default as construction } from "./construction.png";
--- client/src/pages/ErrorWidth.jsx
+++ client/src/pages/ErrorWidth.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { construction } from "../assets/images";
 
 const ErrorWidth = (props) => {
 	return (
-		<div className="hidden lg:block">
-			<p>We are currently building this site for PCs.</p>
-			<p>For now, this page could be best viewed on mobiles!</p>
+		<div className="hidden lg:block text-center mx-auto">
+			<img
+				src={construction}
+				alt="Page under construction"
+				className=" w-2/6 mx-auto"
+			/>
+			<p className="text-5xl font-regular">Page under construction!</p>
+			<p className="text-2xl font-regular">
+				We are currently building this site for PCs.
+			</p>
+			<p className="text-xl font-regular">
+				For now, this page could be best viewed on mobiles!
+			</p>
 		</div>
 	);
 };
