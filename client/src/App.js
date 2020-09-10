e97db8b19e3a8e38c7996abef1753e1056389e71
mchlbataller <mchlbataller@gmail.com>
2020-09-10 13:00:20 +0800

    Supported caching

--- client/src/App.js
+++ client/src/App.js
@@ -38,13 +38,17 @@ function App(props) {
 	const setUserData = useDataStore((state) => state.setUserData);
 
 	React.useEffect(() => {
+		// Use data from cache first
+		const cache = localStorage.getItem("cachedData")
+		setUserData(cache ? JSON.parse(cache) : null)
+
 		const fetchData = async () => {
 			await firebase
 				.firestore()
 				.collection("data")
 				.doc("info")
 				.get()
-				.then((res) => setUserData(res.data()));
+				.then((res) => { setUserData(res.data()); localStorage.setItem("cachedData", JSON.stringify(res.data())) });
 		};
 		fetchData();
 		// eslint-disable-next-line
