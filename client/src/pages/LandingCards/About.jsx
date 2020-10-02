66885ff2adfa7d44925acb7a01f3473477afb3a3
mchlbataller <mchlbataller@gmail.com>
2020-10-02 09:16:05 +0800

    Removed styles in About.jsx

--- client/src/pages/LandingCards/About.jsx
+++ client/src/pages/LandingCards/About.jsx
@@ -1,30 +1,31 @@
 import "aos/dist/aos.css"; // You can also use <link> for styles
 
-import React, { useContext } from "react";
-
 import AOS from "aos";
-import { ThemeContext } from "ThemeProvider";
-import { User } from "assets/icons";
+import React from "react";
+import { User as UserIcon } from "assets/icons";
 import styled from "styled-components";
 import { useDataStore } from "state/data";
 
 AOS.init();
 
+const StyledSection = styled.section.attrs({
+	className:
+		"flex items-center justify-center flex-col w-full px-5 lg:px-0 lg:w-1/2 mx-auto",
+})`
+	min-height: 80vh;
+	font-family: "Open Sans", sans-serif;
+`;
+
 export const About = () => {
 	const data = useDataStore((state) => state.userData);
-	const theme = useContext(ThemeContext);
 	return (
-		<section
-			className="flex items-center justify-center flex-col w-full px-5 lg:px-0 lg:w-1/2 mx-auto"
-			style={{ height: "80vh" }}
-		>
-			<User
+		<StyledSection>
+			<UserIcon
 				style={{ width: "70px", fill: "#9ADBFF" }}
 				data-aos="fade-up"
 			/>
 			<h1
 				className="text-center text-lg lg:text-3xl font-bold text-blue-500"
-				style={{ fontFamily: "Open Sans" }}
 				data-aos="fade-up"
 			>
 				{data?.about_header}
@@ -32,15 +33,12 @@ export const About = () => {
 			<br />
 			<div data-aos="fade-up">
 				{data.about_text.map((text) => (
-					<p
-						className="mb-3 text-center text-gray-300 text-sm lg:text-base"
-						style={{ fontFamily: "Open Sans" }}
-					>
+					<p className="mb-3 text-center text-gray-300 text-sm lg:text-base">
 						{text}
 					</p>
 				))}
 			</div>
-		</section>
+		</StyledSection>
 	);
 };
 
